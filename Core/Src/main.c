b30c7b08e7d0441ad402a57f8a052b7945f34a7d
weiliangng <46848740+weiliangng@users.noreply.github.com>
2026-02-14 07:28:52 +0800

    add pulldown

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1078,7 +1078,7 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PC14 PC15 */
