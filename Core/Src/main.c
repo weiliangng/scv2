28fc31dab6ecc95a352b4848aed9afeebcd1f4e7
ngwei <ngweiliang787@gmail.com>
2026-08-16 00:27:10 +0800

    init PA10 LED to off on boot

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1062,7 +1062,7 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOB, GPIO_DIR_Pin|GPIO_RVSOFF_Pin|GPIO_MODELSB_Pin|GPIO_NSIL_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_SWEN_Pin|GPIO_MODEMSB_Pin, GPIO_PIN_RESET);
